a49d76e801691bd2e9cd4849dca94a555eb000ce
Evan <evan.harte@keyin.com>
2024-06-11 16:23:12 -0230

    can now search and get index by using route parameters to find API data

--- src/main/java/com/keyin/hello/GreetingController.java
+++ src/main/java/com/keyin/hello/GreetingController.java
@@ -1,19 +1,41 @@
 package com.keyin.hello;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.websocket.server.PathParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController // need this to be able to access say http://localhost:8080/hello
 @CrossOrigin
 public class GreetingController {
-    @GetMapping("hello")
-    public Greeting greeting(@RequestParam("name") String name) { // allows use of query in URL (?name=Evan)
-        Greeting greeting = new Greeting();
-        greeting.setGreeting("Hellooo");
-        greeting.setName(name);
+    // bridges java code with http layer
+    @Autowired
+    private GreetingService greetingService;
 
-        return greeting;
+    @GetMapping("hello")
+    public Greeting greeting(@RequestParam(value = "name", required = false) String name) { // allows use of query in URL (?name=Evan)
+        return greetingService.generateGreeting(name);
+    }
+
+    @GetMapping("search_greeting")
+    public Greeting searchGreeting(@RequestParam(value = "index", required = false) Integer index) {
+        return greetingService.getGreeting(index);
+    }
+
+    @GetMapping("greetings")
+    public List<Greeting> getAllGreetings() {
+        return greetingService.getAllGreetings();
+    }
+
+    // can do it this way or can do the same as above @GetMapping("search_greeting") - two ways to do the same thing
+    @GetMapping("greeting/{index}")
+    public Greeting getGreeting(@PathVariable Integer index) {
+        return greetingService.getGreeting(index);
+    }
+
+    @PostMapping("greeting")
+    public Greeting createGreeting(@RequestBody Greeting newGreeting) {
+        return greetingService.createGreeting(newGreeting);
     }
 }
---
+++ src/main/java/com/keyin/hello/GreetingService.java
@@ -0,0 +1,39 @@
+package com.keyin.hello;
+
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class GreetingService {
+    private Map<Integer, Greeting> greetingMap = new HashMap<Integer, Greeting>();
+
+    public Greeting generateGreeting(String name) {
+        Greeting greeting = new Greeting();
+        greeting.setGreeting("Hellooo");
+        greeting.setName(name);
+
+        if (StringUtils.hasText(name)) {
+            greeting.setName(name);
+        } else {
+            greeting.setName("World");
+        }
+        return greeting;
+    }
+
+    public Greeting getGreeting(Integer index) {
+        return greetingMap.get(index);
+    }
+
+    public Greeting createGreeting(Greeting newGreeting) {
+        greetingMap.put(greetingMap.size() + 1, newGreeting);
+        return newGreeting;
+    }
+
+    public List<Greeting> getAllGreetings() {
+        return List.copyOf(greetingMap.values());
+    }
+}
