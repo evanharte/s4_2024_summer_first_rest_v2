4815e426a3a1b4fe6c9df1303eeff6362b3ca910
Evan Harte <126525806+evanharte@users.noreply.github.com>
2024-07-04 23:42:59 -0230

    successfully connected to database.

--- src/main/java/com/keyin/hello/Greeting.java
+++ src/main/java/com/keyin/hello/Greeting.java
@@ -1,10 +1,18 @@
 package com.keyin.hello;
 
+import jakarta.persistence.*;
+
 import java.util.List;
 
+@Entity
 public class Greeting {
+    @Id
+    @SequenceGenerator(name = "greeting_sequence", sequenceName = "greeting_sequence", allocationSize = 1, initialValue = 1)
+    @GeneratedValue(generator = "greeting_sequence")
+    private Long id;
     private String greeting;
     private String name;
+    @ManyToMany
     private List<Language> languages;
 
     public String getGreeting() {
--- src/main/java/com/keyin/hello/Language.java
+++ src/main/java/com/keyin/hello/Language.java
@@ -1,6 +1,16 @@
 package com.keyin.hello;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+
+@Entity
 public class Language {
+    @Id
+    @SequenceGenerator(name = "language_sequence", sequenceName = "language_sequence", allocationSize = 1, initialValue = 1)
+    @GeneratedValue(generator = "language_sequence")
+    private Long id;
     private String name;
 
     public Language() {
@@ -13,5 +23,13 @@ public class Language {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
     }
 }
